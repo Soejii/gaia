09364b8ad8a4676305b64b35a1cf79858f082a98
Soejii <ninkaishou27@gmail.com>
2025-06-24 20:50:25 +0700

    chore: adding intl

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:gaia/app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gaia/config.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
 void main() {
-  runApp(
-    const ProviderScope(
-      child: MyApp(),
+  const env =
+      String.fromEnvironment('ENV', defaultValue: Environments.PRODUCTION);
+  debugPrint('✅ Running in ENV: $env');
+  initializeDateFormatting('id_ID').then(
+    (value) => runApp(
+      const ProviderScope(
+        child: MyApp(),
+      ),
     ),
   );
 }
