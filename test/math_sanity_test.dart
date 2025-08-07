6c734df7d69419864bda38e255cd18018d0e2281
Soejii <ninkaishou27@gmail.com>
2025-08-07 11:04:30 +0700

    feat: added flutter unit testing

---
+++ test/math_sanity_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_test/flutter_test.dart';
+
+int add(int a, int b) {
+  return a + b;
+}
+
+void main() {
+  test(
+    'SANITY TEST',
+    () {
+      expect(add(2, 3), 5);
+    },
+  );
+}
