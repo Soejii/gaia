7be0a59b566022957131afc9c86dbed6392d287d
Soejii <ninkaishou27@gmail.com>
2025-08-12 14:39:36 +0700

    feat: added throw to failure in profile and school remote data source

--- lib/features/profile/data/datasource/profile_remote_datasource.dart
+++ lib/features/profile/data/datasource/profile_remote_datasource.dart
@@ -2,13 +2,18 @@ import 'package:dio/dio.dart';
 import 'package:gaia/features/profile/data/mappers/profile_mapper.dart';
 import 'package:gaia/features/profile/data/models/profile_model.dart';
 import 'package:gaia/features/profile/domain/entities/profile_entity.dart';
+import 'package:gaia/shared/core/types/exception.dart';
 
 class ProfileRemoteDatasource {
   final Dio _dio;
   ProfileRemoteDatasource(this._dio);
 
   Future<ProfileEntity> getProfile() async {
-    final res = await _dio.get('/profile');
-    return ProfileModel.fromJson(res.data['data']).toEntity();
+    try {
+      final res = await _dio.get('/profile');
+      return ProfileModel.fromJson(res.data['data']).toEntity();
+    } on DioException catch (e) {
+      throw e.toFailure();
+    }
   }
 }
--- lib/features/school/data/datasource/school_remote_data_source.dart
+++ lib/features/school/data/datasource/school_remote_data_source.dart
@@ -2,13 +2,18 @@ import 'package:dio/dio.dart';
 import 'package:gaia/features/school/data/mappers/school_mapper.dart';
 import 'package:gaia/features/school/data/models/school_model.dart';
 import 'package:gaia/features/school/domain/entities/school_entity.dart';
+import 'package:gaia/shared/core/types/exception.dart';
 
 class SchoolRemoteDataSource {
   final Dio _dio;
   SchoolRemoteDataSource(this._dio);
 
   Future<SchoolEntity> getSchool() async {
-    final res = await _dio.get('/school-profile');
-    return SchoolModel.fromJson(res.data['data']).toEntity();
+    try {
+      final res = await _dio.get('/school-profile');
+      return SchoolModel.fromJson(res.data['data']).toEntity();
+    } on DioException catch (e) {
+      throw e.toFailure();
+    }
   }
 }
