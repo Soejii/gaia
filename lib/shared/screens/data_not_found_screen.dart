1780a90a3e093d40cc0b4b75b024c09f0cd78f2c
Soejii <ninkaishou27@gmail.com>
2025-08-18 14:54:23 +0700

    feat: minor fix on data not found screen

--- lib/shared/screens/data_not_found_screen.dart
+++ lib/shared/screens/data_not_found_screen.dart
@@ -4,23 +4,31 @@ import 'package:gaia/shared/core/constant/app_colors.dart';
 import 'package:gaia/shared/core/constant/assets_helper.dart';
 
 class DataNotFoundScreen extends StatelessWidget {
-  const DataNotFoundScreen({super.key,required this.dataType});
+  const DataNotFoundScreen({super.key, required this.dataType});
   final String dataType;
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Image.asset(AssetsHelper.imgDataNotFound),
-        Text(
-          'Data $dataType Tidak Tersedia',
-          style: TextStyle(
-            fontFamily: 'OpenSans',
-            fontSize: 24.sp,
-            fontWeight: FontWeight.w600,
-            color: AppColors.inactiveColor,
+        SizedBox(height: 100.h),
+        SizedBox(
+          height: 300.h,
+          width: 300.w,
+          child: Image.asset(AssetsHelper.imgDataNotFound),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 40.w),
+          child: Text(
+            'Data $dataType Tidak Tersedia',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontFamily: 'OpenSans',
+              fontSize: 24.sp,
+              fontWeight: FontWeight.w600,
+              color: AppColors.inactiveColor,
+            ),
           ),
         )
       ],
