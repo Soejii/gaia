fd1186f88dc1d6409f16e382718ff414f9a2ea9d
Soejii <ninkaishou27@gmail.com>
2025-08-18 09:52:09 +0700

    feat: added loading into all of image.network

--- lib/features/announcement/presentation/widgets/detail_announcement_image.dart
+++ lib/features/announcement/presentation/widgets/detail_announcement_image.dart
@@ -22,6 +22,16 @@ class DetailAnnouncementImage extends StatelessWidget {
           errorBuilder: (context, error, stackTrace) => Image.asset(
             AssetsHelper.imgAnnouncementPlaceholder,
           ),
+            loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress == null) return child;
+                  return Container(
+                    height: 200.h,
+                    color: Colors.white,
+                    child: const Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  );
+                },
         ),
       ),
     );
--- lib/features/home/presentation/widgets/digital_magazines_card.dart
+++ lib/features/home/presentation/widgets/digital_magazines_card.dart
@@ -42,6 +42,16 @@ class DigitalMagazinesCard extends StatelessWidget {
                 errorBuilder: (context, error, stackTrace) => Image.asset(
                   AssetsHelper.imgAnnouncementPlaceholder,
                 ),
+                  loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress == null) return child;
+                  return Container(
+                    height: 200.h,
+                    color: Colors.white,
+                    child: const Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  );
+                },
               ),
             ),
           ),
