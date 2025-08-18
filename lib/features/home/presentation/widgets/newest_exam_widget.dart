70c948c415d1f288ed927ca0f1bc4d67de5440d0
Soejii <ninkaishou27@gmail.com>
2025-08-18 14:37:46 +0700

    feat: added routing to activity tab from newest widget

--- lib/features/home/presentation/widgets/newest_exam_widget.dart
+++ lib/features/home/presentation/widgets/newest_exam_widget.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gaia/features/activity/domain/type/exam_type.dart';
+import 'package:gaia/features/activity/presentation/providers/activity_providers.dart';
 import 'package:gaia/features/activity/presentation/providers/exam_controller.dart';
 import 'package:gaia/features/activity/presentation/widgets/exam_card.dart';
 import 'package:gaia/features/activity/presentation/widgets/exam_error_card.dart';
 import 'package:gaia/features/activity/presentation/widgets/exam_skeleton_card.dart';
 import 'package:gaia/shared/core/constant/app_colors.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class NewestExamWidget extends ConsumerWidget {
@@ -13,53 +15,55 @@ class NewestExamWidget extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-        final examAsync =
-        ref.watch(examControllerProvider(ExamType.exam));
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 20.w),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                'Latihan Soal Tebaru',
-                style: TextStyle(
-                  fontFamily: 'OpenSans',
-                  fontSize: 16.sp,
-                  fontWeight: FontWeight.w600,
-                  color: AppColors.mainText,
-                ),
-              ),
-              TextButton(
-                onPressed: () {},
-                child: Text(
-                  'Lihat Semua',
-                  style: TextStyle(
-                    fontFamily: 'OpenSans',
-                    fontSize: 12.sp,
-                    fontWeight: FontWeight.w400,
-                    color: AppColors.mainColorSidigs,
+    final examAsync = ref.watch(examControllerProvider(ExamType.exam));
+    return examAsync.when(
+      data: (data) => data.items.isNotEmpty
+          ? Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.w),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Latihan Soal Tebaru',
+                        style: TextStyle(
+                          fontFamily: 'OpenSans',
+                          fontSize: 16.sp,
+                          fontWeight: FontWeight.w600,
+                          color: AppColors.mainText,
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          ref.read(activityTabIndexProvider.notifier).set(0);
+                          context.goNamed('activity');
+                        },
+                        child: Text(
+                          'Lihat Semua',
+                          style: TextStyle(
+                            fontFamily: 'OpenSans',
+                            fontSize: 12.sp,
+                            fontWeight: FontWeight.w400,
+                            color: AppColors.mainColorSidigs,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ),
+                  SizedBox(height: 10.h),
+                  ExamCard(entity: data.items[0])
+                ],
               ),
-            ],
-          ),
-          SizedBox(height: 10.h),
-          examAsync.when(
-            data: (data) => data.items.isNotEmpty
-                ? ExamCard(entity: data.items[0])
-                : const SizedBox.shrink(),
-            error: (e, _) => ExamErrorCard(
-              error: e,
-              onRetry: () => ref.invalidate(examControllerProvider),
-            ),
-            loading: () => const ExamSkeletonCard(),
-          ),
-        ],
+            )
+          : const SizedBox.shrink(),
+      error: (e, _) => ExamErrorCard(
+        error: e,
+        onRetry: () => ref.invalidate(examControllerProvider),
       ),
+      loading: () => const ExamSkeletonCard(),
     );
   }
 }
--- lib/features/home/presentation/widgets/newest_quiz_widget.dart
+++ lib/features/home/presentation/widgets/newest_quiz_widget.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gaia/features/activity/domain/type/exam_type.dart';
+import 'package:gaia/features/activity/presentation/providers/activity_providers.dart';
+import 'package:gaia/features/activity/presentation/providers/exam_controller.dart';
+import 'package:gaia/features/activity/presentation/widgets/exam_error_card.dart';
+import 'package:gaia/features/activity/presentation/widgets/exam_skeleton_card.dart';
 import 'package:gaia/features/home/presentation/widgets/quiz_card.dart';
 import 'package:gaia/shared/core/constant/app_colors.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class NewestQuizWidget extends StatelessWidget {
+class NewestQuizWidget extends ConsumerWidget {
   const NewestQuizWidget({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final quizAsync = ref.watch(examControllerProvider(ExamType.quiz));
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 20.w),
       child: Column(
@@ -27,7 +35,10 @@ class NewestQuizWidget extends StatelessWidget {
                 ),
               ),
               TextButton(
-                onPressed: () {},
+                onPressed: () {
+                  ref.read(activityTabIndexProvider.notifier).set(1);
+                  context.goNamed('activity');
+                },
                 child: Text(
                   'Lihat Semua',
                   style: TextStyle(
@@ -41,7 +52,16 @@ class NewestQuizWidget extends StatelessWidget {
             ],
           ),
           SizedBox(height: 10.h),
-          const QuizCard(),
+          quizAsync.when(
+            data: (data) => data.items.isNotEmpty
+                ? QuizCard(entity: data.items[0])
+                : const SizedBox.shrink(),
+            error: (e, _) => ExamErrorCard(
+              error: e,
+              onRetry: () => ref.invalidate(examControllerProvider),
+            ),
+            loading: () => const ExamSkeletonCard(),
+          ),
         ],
       ),
     );
