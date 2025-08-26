0cc9b46d5414657d7d7273a9ec4c9e2cbad7abef
Soejii <ninkaishou27@gmail.com>
2025-08-27 00:17:38 +0700

    fix: fixed text didnt align properly in exam and quiz card, move quiz card widget into activity feature

--- lib/features/activity/presentation/widgets/exam_content_widget.dart
+++ lib/features/activity/presentation/widgets/exam_content_widget.dart
@@ -4,7 +4,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gaia/features/activity/domain/type/exam_type.dart';
 import 'package:gaia/features/activity/presentation/providers/exam_controller.dart';
 import 'package:gaia/features/activity/presentation/widgets/exam_card.dart';
-import 'package:gaia/features/home/presentation/widgets/quiz_card.dart';
+import 'package:gaia/features/activity/presentation/widgets/quiz_card.dart';
 import 'package:gaia/shared/screens/data_not_found_screen.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
