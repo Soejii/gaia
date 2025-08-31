b28480c055b98b23915fe992b4574502adbb8aab
zhafrans <bebenmerayap@gmail.com>
2025-08-31 14:39:56 +0700

    refactor: add accountinfo item class for type saftey and move data iteration to screen using listview.builder

--- lib/features/profile/presentation/screens/account_information_screen.dart
+++ lib/features/profile/presentation/screens/account_information_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gaia/shared/widgets/custom_app_bar_widget.dart';
 import 'package:gaia/features/profile/presentation/providers/profile_controller.dart';
 import 'package:gaia/features/profile/presentation/widgets/account_information_content.dart';
@@ -19,7 +20,28 @@ class AccountInformationScreen extends ConsumerWidget {
         leadingIcon: true,
       ),
       body: profileAsync.when(
-        data: (profile) => AccountInformationContent(profile: profile),
+        data: (profile) {
+          final accountItems = [
+            AccountInfoItem(label: 'Nama Lengkap', value: profile.name),
+            AccountInfoItem(label: 'Username', value: profile.username),
+            AccountInfoItem(label: 'Email', value: profile.email),
+            AccountInfoItem(label: 'NIS', value: profile.nis),
+            AccountInfoItem(label: 'NISN', value: profile.nisn),
+            AccountInfoItem(label: 'Tempat dan Tanggal Lahir', value: '${profile.birthplace}, ${profile.birthdate}'),
+            AccountInfoItem(label: 'Jenis Kelamin', value: profile.gender),
+            AccountInfoItem(label: 'Agama', value: profile.religion),
+            AccountInfoItem(label: 'Alamat Sesuai KTP', value: profile.address),
+            AccountInfoItem(label: 'Asal Sekolah', value: profile.schoolOrigin),
+          ];
+
+          return ListView.builder(
+            padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
+            itemCount: accountItems.length,
+            itemBuilder: (context, index) {
+              return AccountInfoField(item: accountItems[index]);
+            },
+          );
+        },
         loading: () => const Center(
           child: CircularProgressIndicator(
             color: AppColors.mainColorSidigs,
