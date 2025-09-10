933e3f3a0a377d89ef95086ebcdc0ea76c75cee9
zhafrans <bebenmerayap@gmail.com>
2025-09-10 09:08:07 +0700

    fix: improve emoney history controller pagination
    - Enhance pagination function per owner feedback
    - Update error handling in loadMore()

--- lib/features/balances/presentation/providers/emoney_history_controller.g.dart
+++ lib/features/balances/presentation/providers/emoney_history_controller.g.dart
@@ -6,33 +6,13 @@ part of 'emoney_history_controller.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$getEmoneyHistoryUsecaseHash() =>
-    r'7104f3d4de53221cbf091b84bf274ca41c07951f';
-
-/// See also [getEmoneyHistoryUsecase].
-@ProviderFor(getEmoneyHistoryUsecase)
-final getEmoneyHistoryUsecaseProvider =
-    AutoDisposeProvider<GetEmoneyHistoryUsecase>.internal(
-  getEmoneyHistoryUsecase,
-  name: r'getEmoneyHistoryUsecaseProvider',
-  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-      ? null
-      : _$getEmoneyHistoryUsecaseHash,
-  dependencies: null,
-  allTransitiveDependencies: null,
-);
-
-@Deprecated('Will be removed in 3.0. Use Ref instead')
-// ignore: unused_element
-typedef GetEmoneyHistoryUsecaseRef
-    = AutoDisposeProviderRef<GetEmoneyHistoryUsecase>;
 String _$emoneyHistoryControllerHash() =>
-    r'02079eed9b141dc791a60bb2653b704f00d5b83c';
+    r'49602b31aacf22915c1f3ab18fbaadcd6781a6e8';
 
 /// See also [EmoneyHistoryController].
 @ProviderFor(EmoneyHistoryController)
-final emoneyHistoryControllerProvider = AutoDisposeAsyncNotifierProvider<
-    EmoneyHistoryController, List<EmoneyHistoryEntity>>.internal(
+final emoneyHistoryControllerProvider = AutoDisposeNotifierProvider<
+    EmoneyHistoryController, AsyncValue<Paged<EmoneyHistoryEntity>>>.internal(
   EmoneyHistoryController.new,
   name: r'emoneyHistoryControllerProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
@@ -43,6 +23,6 @@ final emoneyHistoryControllerProvider = AutoDisposeAsyncNotifierProvider<
 );
 
 typedef _$EmoneyHistoryController
-    = AutoDisposeAsyncNotifier<List<EmoneyHistoryEntity>>;
+    = AutoDisposeNotifier<AsyncValue<Paged<EmoneyHistoryEntity>>>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
