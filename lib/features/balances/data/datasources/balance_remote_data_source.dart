59d16ad0780355793d7aaed6f869dc2cb871dd8b
zhafrans <bebenmerayap@gmail.com>
2025-09-10 09:15:54 +0700

    feat: add savings data source methods
    - Add getSavingsHistory method with pagination support
    - Add getSavingsBalance method for savings detail
    - Support limit and offset parameters for pagination

--- lib/features/balances/data/datasources/balance_remote_data_source.dart
+++ lib/features/balances/data/datasources/balance_remote_data_source.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:gaia/features/balances/data/models/emoney_model.dart';
 import 'package:gaia/features/balances/data/models/emoney_history_model.dart';
 import 'package:gaia/features/balances/data/models/savings_model.dart';
+import 'package:gaia/features/balances/data/models/savings_history_model.dart';
 
 class BalanceRemoteDataSource {
   final Dio _dio;
@@ -13,21 +14,37 @@ class BalanceRemoteDataSource {
     return data;
   }
 
-  Future<List<EmoneyHistoryModel>> getEmoneyHistory() async {
-    final res = await _dio.get('/bill/history-emoney');
+  Future<List<EmoneyHistoryModel>> getEmoneyHistory({int? page}) async {
+    final offset = page != null ? (page - 1) * 10 : 0;
+    final res = await _dio.get(
+      '/bill/history-emoney',
+      queryParameters: {
+        'limit': 10,
+        'offset': offset,
+      },
+    );
     final List<dynamic> emoneyData = res.data['data']['emoney'];
     return emoneyData.map((json) => EmoneyHistoryModel.fromJson(json)).toList();
   }
 
-    Future<SavingsModel> getSavingsBalance() async {
-   
-    await Future.delayed(const Duration(milliseconds: 500));
-    return const SavingsModel(
-      name:'anjay',
-      cardId: 'TABUNGAN-12345',
-      saldoTabungan: 'Rp. 2.500.000',
-      totalDebit: 'Rp. 5.000.000',
-      totalKredit: 'Rp. 2.500.000',
+  Future<SavingsModel> getSavingsBalance() async {
+    final res = await _dio.get('/topup/get-detail-topup-student');
+    final data = SavingsModel.fromJson(res.data['data']);
+    return data;
+  }
+
+  Future<List<SavingsHistoryModel>> getSavingsHistory({int? page}) async {
+    final offset = page != null ? (page - 1) * 10 : 0;
+    final res = await _dio.get(
+      '/topup/get-detail-byStudentId',
+      queryParameters: {
+        'limit': 10,
+        'offset': offset,
+      },
     );
+    final List<dynamic> savingsData = res.data['data'];
+    return savingsData
+        .map((json) => SavingsHistoryModel.fromJson(json))
+        .toList();
   }
 }
