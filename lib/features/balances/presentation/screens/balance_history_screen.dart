1d676fd9f6f3ec88355d03937ab2c02e2d603b31
zhafrans <bebenmerayap@gmail.com>
2025-09-12 22:36:29 +0700

    enhance: using extension to customize app bar based on type instead of if -else

--- lib/features/balances/presentation/screens/balance_history_screen.dart
+++ lib/features/balances/presentation/screens/balance_history_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gaia/features/balances/domain/type/balance_type.dart';
+import 'package:gaia/features/balances/domain/type/history_balance_type_extension.dart';
 import 'package:gaia/features/balances/presentation/widgets/balance_history_list_widget.dart';
 import 'package:gaia/shared/widgets/custom_app_bar_widget.dart';
 
@@ -14,13 +15,9 @@ class BalanceHistoryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final title = type == BalanceType.emoney
-        ? 'Riwayat Transaksi E-Money'
-        : 'Riwayat Transaksi Tabungan';
-
     return Scaffold(
       extendBodyBehindAppBar: true,
-      appBar: CustomAppBarWidget(title: title, leadingIcon: true),
+      appBar: CustomAppBarWidget(title: type.historyTitle, leadingIcon: true),
       body: Padding(
         padding: EdgeInsets.all(16.w),
         child: BalanceHistoryListWidget(type: type),
